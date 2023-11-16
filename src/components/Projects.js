69baaa8aee60b4137c7c3c47f20f1e21735c338e
Hicham Roldan <82762193+Hicham017@users.noreply.github.com>
2023-11-16 16:30:47 +0100

    onglets

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -104,13 +104,13 @@ export const Projects = () => {
                       id="pills-tab"
                     >
                       <Nav.Item>
-                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                        <Nav.Link eventKey="first">Onglet 1</Nav.Link>
                       </Nav.Item>
                       <Nav.Item>
-                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                        <Nav.Link eventKey="second">Onglet 2</Nav.Link>
                       </Nav.Item>
                       <Nav.Item>
-                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                        <Nav.Link eventKey="third">Onglet 3</Nav.Link>
                       </Nav.Item>
                     </Nav>
                     <Tab.Content
